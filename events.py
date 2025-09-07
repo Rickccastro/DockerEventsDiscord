93e91cddcdbec24c1342f3aa7aeac32a535dd109
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-09-07 20:02:50 -0300

    feat: filtrando o evento die dos containers

--- events.py
+++ events.py
@@ -2,5 +2,8 @@ import docker
 
 client = docker.DockerClient(base_url='npipe:////./pipe/docker_engine')
 
-for event in client.events(decode=True):
-    print(event)
+for event in client.events(decode=True,filters = {"event": "die"}):
+    container_id = event["id"]
+    container_name = event["Actor"]["Attributes"]["name"]
+    epoch_time = event["time"]  
+    print("O container,%s (%s) foi finalizado às %s" % (container_name, container_id, epoch_time))
